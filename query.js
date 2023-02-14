7aa17402616cf366f481f7a0032bb2956cccd3cb
nathan-getz <97640234+nathan-getz@users.noreply.github.com>
2023-02-13 20:44:30 -0700

    Update query.js

--- query.js
+++ query.js
@@ -39,8 +39,7 @@ const CSCI2270_CurrentRecitationRepo = {
 
 const urlParams = new URL(window.location.toLocaleString()).searchParams;
 const id = urlParams.get("id").toString();
-const username = urlParams.get("username");
-username = (username == null) ? "nathan-getz" : username.toString();
+const username = (urlParams.get("username") == null) ? "nathan-getz" : urlParams.get("username").toString();
 
 /**
  * @param {String} id
